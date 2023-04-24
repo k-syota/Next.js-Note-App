5641dc6cc1e08300581135a4734e26d4d9c9574a
Syota Nishimura <syota4723@gmail.com>
2023-04-24 21:02:29 +0900

    :sparkles: ノートの一覧表示を実装

--- app/type.ts
+++ app/type.ts
@@ -1,17 +1,9 @@
 import { z } from "zod";
 
-export const zNote = z.object({
-  id: z.number().int(),
-  title: z.string(),
-  body: z.string(),
-  createdAt: z.string().datetime(),
-  updatedAt: z.string().datetime(),
+export const zVersion = z.string().regex(/^\d+\.\d+\.\d+$/);
+export const zSettings = z.object({
+  version: zVersion,
+  faq: z.string(),
+  tos: z.string(),
 });
-export const zNotes = z.array(zNote);
-export const zUpsertNote = z.object({
-  title: z.string(),
-  body: z.string(),
-});
-
-export type Note = z.infer<typeof zNote>;
-export type Notes = z.infer<typeof zNotes>;
+export type Settings = z.infer<typeof zSettings>;
