417f77ad4dffe23e59744e73347a700b37626f8c
Syota Nishimura <syota4723@gmail.com>
2023-04-25 15:58:18 +0900

    :sparkles: ノートの新規作成用APIを実装

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -1,5 +1,6 @@
+import { zUpsertNote } from "@/app/notes/type";
 import { prisma } from "@/globals/db";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 // 1. 動的レンダリングを強制する
 export const dynamic = "force-dynamic";
@@ -9,4 +10,12 @@ export async function GET() {
   // 3. DBからノート一覧を取得
   const notes = await prisma.note.findMany();
   return NextResponse.json(notes);
+}
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+  const parcedData = zUpsertNote.parse(data);
+  const note = await prisma.note.create({
+    data: { title: parcedData.title, body: parcedData.body },
+  });
+  return new NextResponse(`${note.id}`, { status: 201 });
 }
